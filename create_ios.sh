6bb1d5b4b9fe840ace1cd6c4c365ebdd08948ee1
Pavel Kononov <depik400@yandex.ru>
2026-07-12 03:36:17 +0700

    feat: Version for ios. Low perfomance

--- create_ios.sh
+++ create_ios.sh
@@ -12,3 +12,17 @@ go run gen_web.go
 cd server/cmd 
 
 ~/go/bin/fyne package -os ios -app-id org.yourok.torrserver --name TorrServer -icon ../../web/public/icon.png
+
+cd ..
+mkdir -p ../builds/ios
+mv cmd/TorrServer.app ../builds/ios/TorrServer.app
+
+rm -rf ../builds/ios/Payload
+mkdir -p ../builds/ios/Payload
+cp -R ../builds/ios/TorrServer.app ../builds/ios/Payload/TorrServer.app
+
+cd ../builds/ios
+rm -f TorrServer.ipa
+zip -qry TorrServer.ipa Payload
+rm -rf Payload
+rm -rf TorrServer.app
--- create_mac_os.sh
+++ create_mac_os.sh
@@ -11,3 +11,6 @@ go run gen_web.go
 
 cd server
 CGO_ENABLED=1 GOARCH=arm64 ~/go/bin/fyne package --name "TorrServer" -icon ../../web/public/icon.png --src=./cmd
+
+mkdir -p ../builds/macOS
+mv TorrServer.app ../builds/macOS/TorrServer.app
